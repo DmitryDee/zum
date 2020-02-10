aa715e2dfd31affd5f947b89750cb7b5a99a9c9b
Di.Dmitry <ddidenko@lipisinov.net>
2020-02-10 15:02:15 +0300

    last work 15-01 02.10.2020

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,46 @@
 $(document).ready(function(){
-  // $('success-slider__year-holder').slick({
-  //   infinite: false,
-  //   fade: false,
-  //   slidesToShow: 3,
-  //   slidesToScroll: 1,
-  //   centerMode: true,
-  //   dots: false,
-  //   arrows: false,
-  // });
+  // helper
+  function setData(){
+    var curSlide = $('.slick-current .success-slider__item').attr('data-year');
+     $('.cur-year').text(curSlide);
+  }
 
+  function nextYear(){
+    $('.success-slider__year-holder').slick({
+      infinite: true,
+      fade: false,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.success-slider__items-list'
+    });  
+  }
 
   $('.success-slider__items-list').slick({
     infinite: true,
     fade: true,
     dots: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    draggable: false,
+    arrows: true,
+    asNavFor: '.success-slider__year-holder',
     appendArrows: $('.success-slider__year-nav'),
     prevArrow: "<span class=\"arrow arrow_prev-year\"></span>",
     nextArrow: "<span class=\"arrow arrow_next-year\"></span>",
     responsive: [
       {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
         breakpoint: 375,
         settings: {
           arrows: false,
-          slidesToShow: 1,
-          slidesToScroll: 1
         }
       }
     ]
+  });
+
+  $('.arrow').on('click', function(){
+    setData();
+  });
+
+  $('.success-slider__items-list').on('swipe', function(event, slick, direction){
+    setData();
   });
   
   $('.achievements__tab-item').click(function(e){
@@ -57,4 +62,6 @@ $(document).ready(function(){
       }
     }    
   });
+
+  nextYear();
 });
